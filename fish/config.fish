0a894d748b2bfdca3622af02cd7bf9b50a7163dd
William <william_silva2003@hotmail.com>
2024-12-17 00:11:35 -0300

    update nsync function

--- fish/config.fish
+++ fish/config.fish
@@ -22,7 +22,7 @@ end
 
 function nsync
     cd $HOME/flake
-    doas chmod -R go+rw .git/ overlays/ pkgs/ configuration.nix flake.lock flake.nix hardware-configuration.nix home.nix
+    doas chmod -R go+rw .git/ home_manager/ nixos/ overlays/ pkgs/ flake.lock flake.nix nixpkgs.nix shell.nix
     doas chown -R root .
     nix flake update
     doas nixos-rebuild switch --flake .
